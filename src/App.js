ae0c7dc372b023be4caf9e0a7881300ffa82d478
indrajeet420 <indrajeet420@users.noreply.github.com>
2022-04-05 01:38:41 +0530

    JSON 1.1

--- src/App.js
+++ src/App.js
@@ -4,40 +4,74 @@ import Header from "./component/Header";
 import ShowComment from "./component/ShowComment";
 import { v4 as uuidv4 } from "uuid";
 import React, { useState, useEffect } from "react";
+import api from "./API/Comment";
 
 import "./component/feedback.css";
 
 function App() {
-  const KEy_STORAGE = "contacts";
+  // const KEy_STORAGE = "contacts";
+
   const [comments, setComments] = useState([]);
-  const addcommenthandler = (comment) => {
-    setComments([...comments, { id: uuidv4(), ...comment }]);
+
+  const addcommenthandler = async (comment) => {
+    // setComments([...comments, { id: uuidv4(), ...comment }]);
+    const request={
+      id: uuidv4(), 
+      ...comment
+    }
+const response=await api.post("/Comments",request)
+setComments([...comments, response.data]);
+
   };
-  let deletecomment = (id) => {
+  let deletecomment = async (id) => {
+    // const newComments = comments.filter((comment) => {
+    //   return comment.id !== id;
+    await api.delete(`/Comments/${id}`);
     const newComments = comments.filter((comment) => {
-      return comment.id !== id;
-    });
+        return comment.id !== id;  });
     setComments(newComments);
   };
+
+  const retriveComment = async () => {
+    const response = await api.get("/Comments");
+    return response.data;
+  };
   useEffect(() => {
-    const retriveComment = JSON.parse(localStorage.getItem(KEy_STORAGE));
-    if (retriveComment) setComments(retriveComment);
-  }, []);
-  useEffect(() => {
-    localStorage.setItem(KEy_STORAGE, JSON.stringify(comments));
-  }, [comments]);
+    // const retriveComment = JSON.parse(localStorage.getItem(KEy_STORAGE));
+    // if (retriveComment) setComments(retriveComment);
+    const getAllComments = async () => {
+      const retrivedComments = await retriveComment();
+      if (retrivedComments) setComments(retrivedComments);
+    };
+    getAllComments();
+  },[]);
+
 
   return (
     <>
       <div className="body">
         <Header />
+
         <div className="aside">
           <div className="left">
             <Form addcommenthandler={addcommenthandler} />
           </div>
           <div className="right">
-            <h2> Top 5 Comments are.... </h2>
-            <h4>total comment <button style={{padding:"10px",margin:"2px 10px",backgroundColor:"black",color:"white" ,borderRadius:"10px"}} >{comments.length}</button></h4>
+            <h3> Top 5 Comments are.... </h3>
+            <h4>
+              total comment{" "}
+              <button
+                style={{
+                  padding: "10px",
+                  margin: "2px 10px",
+                  backgroundColor: "black",
+                  color: "white",
+                  borderRadius: "10px",
+                }}
+              >
+                {comments.length}
+              </button>
+            </h4>
             <hr></hr>
             <ShowComment comments={comments} deletecomment={deletecomment} />
           </div>
